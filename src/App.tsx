746caf924df8cfd2bf47384447e51a64df19a399
jasoncruz98 <jason@mygurulab.com>
2022-10-25 21:55:45 +0800

    added debounced search bar, loading spinner, improved responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import { useAllSubmissions } from './api/hooks/submission'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import useDebounce from './api/hooks/useDebounce';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Box } from '@mui/material';
+
 
 //TODO:
-// Add a search bar
-// Add data hook to fetch data from https://github.com/pushshift/api
-// Make search bar call data hook
+// [DONE] Add a search bar
+// [DONE] Add data hook to fetch data from https://github.com/pushshift/api
+// [DONE] Make search bar call data hook
 // Add a loading spinner while data is loading
-// Debounce search bar
+// [DONE] Debounce search bar
 // Add infinite scroll
 // Add button to save submission to local storage
 // Add button to remove submission from local storage
 // Add App Bar with button to view saved submissions
 
-function App() {
-  const [count, setCount] = useState(0)
+export interface RedditPost {
+    id: number
+    title: string
+    selftext: string
+    url: string
+}
 
-  const { data } = useAllSubmissions();
+function App() {
+  
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const { data, loading: isLoading, error: isError } = useAllSubmissions(debouncedSearchTerm);
+
+  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearchTerm(value);
+  };
+
 
   return (
     <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-         {data?.map((submission) => (
-           <CardContent sx={{
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <TextField id="outlined-search" label="Search field" type="search" onChange={handleSearchInputChange} />
+        {isError ? <div>Something went wrong...</div> : (
+          isLoading ? <CircularProgress
+          sx={{
+            marginY: 2,
+          }}
+        color="secondary"
+        size={24}
+      /> : (data?.map((post: RedditPost) => (
+        <Card
+          sx={{
+            width: '80%',
+            marginY: 2,
+          }}
+          key={post.id}>
+          <CardContent
+          key={post.id}
+             sx={{
              display: 'flex',
              flexDirection: 'column',
              alignItems: 'center',
@@ -55,14 +78,16 @@ function App() {
              backgroundColor: 'white',
              borderRadius: '1rem',
              boxShadow: '0 0 1rem rgba(0, 0, 0, 0.2)',
-          }}>
-        <div key={submission.id}>
-          <h1>{submission.title}</h1>
-          <p>{submission.selftext}</p>
-          <p>{submission.url}</p>
-          </div>
+          }}
+          >
+            <h2>{post.title}</h2>
+            <p>{post.selftext}</p>
+            <a href={post.url} target="_blank" rel="noreferrer">Link</a>
           </CardContent>
-      ))}
+        </Card>
+      ))))}
+        
+      </Box>
     </div>
   )
 }
---
+++ src/api/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+  console.log("debouncedValue", debouncedValue);
+  return debouncedValue;
+};
+
+export default useDebounce;
